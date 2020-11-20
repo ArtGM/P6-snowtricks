96641d0fcc0f38830dc9dabec5462da19eb82ce2
ArtMG <gx.morisson@protonmail.com>
2020-11-20 09:18:13 +0100

    * Dev: Add confirm password

--- src/Domain/User/RegistrationFormType.php
+++ src/Domain/User/RegistrationFormType.php
@@ -8,6 +8,7 @@ namespace App\Domain\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -34,10 +35,13 @@ class RegistrationFormType extends AbstractType {
 				] )
 			->add(
 				'password',
-				PasswordType::class, [
-				'attr' => [
-					'label' => 'password'
-				]
+				RepeatedType::class, [
+				'type'            => PasswordType::class,
+				'invalid_message' => 'The password fields must match.',
+				'options'         => [ 'attr' => [ 'class' => 'password-field' ] ],
+				'required'        => true,
+				'first_options'   => [ 'label' => 'Password' ],
+				'second_options'  => [ 'label' => 'Repeat Password' ],
 			] );
 	}
 
